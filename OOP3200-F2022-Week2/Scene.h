cfe613827bf9c4cbd2f0d903b2d080be4a199cb2
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-10-07 14:26:04 -0400

    added Scene.h

--- OOP3200-F2022-Week2/Scene.h
+++ OOP3200-F2022-Week2/Scene.h
@@ -1,6 +1,45 @@
 #pragma once
+#ifndef __SCENE__
+#define __SCENE__
+#include <string>
+#include <vector>
+
+#include "DisplayObject.h"
+
 class Scene
 {
-	//TODO: Complete the Scene Class
+public:
+	// Constructor(s)
+	Scene(std::string& name);
+
+	// Destructor
+	~Scene();
+
+	// Scene Life-Cycle Functions
+	void Draw();
+	void Update();
+
+	// Getters and Setters
+	std::string GetName();
+	void SetName(std::string& name);
+
+	// Public Utility Functions
+	void AddChild(DisplayObject* child);
+	void RemoveChild(DisplayObject* child);
+	void RemoveAllChildren();
+
+	// Public Overriden Functions
+	std::string to_string();
+private:
+	// Instance Members
+	std::string m_name;
+	std::vector<DisplayObject*> m_pDisplayList;
+
+	// Private Methods
+	void updateDisplayList();
+	void drawDisplayList();
+
 };
+
+#endif /* defined (__SCENE__) */
 
